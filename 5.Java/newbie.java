bb554673807852ea3e7945e0aec223cda4022145
aadiidaa <84100622+aadiidaa@users.noreply.github.com>
2021-10-01 17:27:51 +0530

    Create newbie.java

---
+++ 5.Java/newbie.java
@@ -0,0 +1,10 @@
+class Lapis {
+    public static void main(String[] args) {
+
+    String name = "Lapis2";
+    int age = 6 ;
+    String username = "aadiidaa";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
