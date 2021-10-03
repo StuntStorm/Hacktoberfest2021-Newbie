4cc23e514f1d4f1ba1861989e9e444fb24921713
Gowrishankar V.V <89328469+Gowrishankarvv@users.noreply.github.com>
2021-10-03 16:30:21 +0530

    Create Gowrishankarvv.c

---
+++ 4.C/Gowrishankarvv.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h> 
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : Gowrishankar\n");
+     printf("Age    : 18\n");
+     printf("Username : Gowrishankarvv\n");
+     return(0);
+  }
