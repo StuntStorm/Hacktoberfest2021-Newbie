ea770567bd635c626fb8c27ffdb0b65d5e3ecca6
Rizwan-Nizarudin <72914440+Rizwan-Nizarudin@users.noreply.github.com>
2021-10-06 08:09:02 +0500

    Create Rizwan-Nizarudin.py

---
+++ 3.Python/Rizwan-Nizarudin.py
@@ -0,0 +1,5 @@
+name, age = "Rizwan", 19
+username = "Rizwan-Nizarudin"
+print ('Haiyaaa Github')
+print("Working at Github be swaggggg!")
+print("Name: {}\nAge: {}\nUsername: {}".format(name, age, username))
